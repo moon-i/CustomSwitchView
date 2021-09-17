78307d0eb2451c77bb4379addc12346dfe755877
moon-i <tjans1102@gmail.com>
2021-09-17 14:59:35 +0900

    create text switch project

---
+++ app/src/main/java/com/shinhan/switchtest/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.shinhan.switchtest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.shinhan.switchtest.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.switchViewBtn.setBackground(R.color.teal_200)
+        binding.switchViewBtn.setStateChangeListener(::switchChange)
+    }
+
+    private fun switchChange(isOn: Boolean) {
+        if (isOn) {
+            Log.i("test", "switch On")
+        } else {
+            Log.i("test", "switch Off")
+        }
+    }
+}
